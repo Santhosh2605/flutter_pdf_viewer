6f16e803ab694a1a0ba7a0857c7e7a225987b2fc
Santhosh R <91540997+Santhosh2605@users.noreply.github.com>
2022-05-27 19:09:18 +0530

    error files uploaded

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:pdf_read/PdfFirebase/FirstPage.dart';
+
+import 'pdfFirebase/FirstPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: FirstPage(),
+    );
+  }
+}
